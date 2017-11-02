c7cfee9f563743d38304bc06efd8097b4c93ed4c
JHerman2004 <james.herman20@gmail.com>
2017-11-02 17:28:04 +0000

    pg137

--- keyword-wrangler/spec/resetDatabase.js
+++ keyword-wrangler/spec/resetDatabase.js
@@ -11,13 +11,13 @@ var resetDatabase = function(dbSession, callback){
 			dbSession.remove('keyword', '1', function(err){
 				callback(err)
 			});
-		};
+		},
 
 		function(callback){
 			dbSession.remove('category', '1', function(err){
 				callback(err)
 			});
-		};
+		}
 
 		],
 
--- keyword-wrangler/src/backend/index.js
+++ keyword-wrangler/src/backend/index.js
@@ -1,18 +1,9 @@
 'use strict';
 
-var Percolator = require('percolator').Percolator;
+var Server = require('./server.js').Server;
 
-var port = 8080;
-var server = Percolator({'port': port});
-
-server.route('/api/keywords',
-{
-	GET: function (req, res) {
-		res.object({'foo': 'bar'}).send()
-	}
-}
-);
+var server = Server(8080);
 
 server.listen(function() {
-	console.log('Server started and listening on port', port);
+	console.log('Server started and listening on port', server.options.port);
 });
